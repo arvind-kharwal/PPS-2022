975e5aede3768258a28433ff75ac62a848c02468
Arvind Kharwal <karvind629@gmail.com>
2022-04-21 12:57:22 +0530

    21 April 2022

---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    long int n,i,a,sum=0;
+    printf("\nEnter the number: ");
+    scanf("%ld",&n);
+    while(n>0)
+    {
+        a = n%10;
+        sum= sum*10+a;
+        n= n/10;
+    }
+    printf("\nThe sum of 5 digits number: %ld",sum);
+    return(0);
+}
